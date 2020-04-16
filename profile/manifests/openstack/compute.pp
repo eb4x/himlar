2fe269f8fb6b39fbaafe4c258e914e312c52a30e
tanzr <tanzr@users.noreply.github.com>
2020-04-16 11:23:05 +0200

    stein/nova

--- profile/manifests/openstack/compute.pp
+++ profile/manifests/openstack/compute.pp
@@ -8,6 +8,7 @@ class profile::openstack::compute(
   include ::nova::config
   include ::nova::placement
   include ::nova::network::neutron
+  include ::logging # test if it works
 
   if $manage_telemetry {
     include ::profile::openstack::telemetry::polling
