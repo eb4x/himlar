115679f893495e405d191ca68ccd27ad82f9d28f
Erik Berg <erikberg@usit.uio.no>
2021-05-13 16:08:22 +0200

    only manage_dns_records when true

--- profile/manifests/network/dns_extra_zone.pp
+++ profile/manifests/network/dns_extra_zone.pp
@@ -3,14 +3,17 @@
 # E.G. foreman_proxy with managed dns server
 #
 class profile::network::dns_extra_zone(
+  Boolean $manage_dns_records  = $profile::network::services::manage_dns_records,
   $zone,
 ) {
 
-  ::dns::zone { $zone:
-    soa     => $::fqdn,
-    reverse => false,
-    soaip   => $::ipaddress_mgmt1,
-    update_policy => {'rndc-key' => {'matchtype' => 'zonesub', 'rr' => 'ANY'}}
+  if $manage_dns_records {
+    ::dns::zone { $zone:
+      soa     => $::fqdn,
+      reverse => false,
+      soaip   => $::ipaddress_mgmt1,
+      update_policy => {'rndc-key' => {'matchtype' => 'zonesub', 'rr' => 'ANY'}}
+    }
   }
 
 }
